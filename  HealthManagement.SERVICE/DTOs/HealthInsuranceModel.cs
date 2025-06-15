fe6650b137845c55f0b8f24f3dce97e9ee7d6919
Saba-Burduli <sabagg790@gmail.com>
2025-06-15 19:47:48 +0400

    Add HealthInsuranceModel DTO
    Introduce a new DTO class, `HealthInsuranceModel`, in the `HealthManagement.SERVICE.DTOs` namespace. This model includes CompanyName, Address, Phone, and Email properties to represent health insurance details.

---
+++  HealthManagement.SERVICE/DTOs/HealthInsuranceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HealthManagement.SERVICE.DTOs;
+
+public class HealthInsuranceModel
+{
+    public string? CompanyName { get; set; }
+    public string? Address { get; set; }
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
+}
