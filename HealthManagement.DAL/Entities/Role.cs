798310321b71c2edec89ab89fbb4fd02bb1b3e60
Saba-Burduli <sabagg790@gmail.com>
2025-06-15 04:53:26 +0400

    Add Role entity to the data access layer
    Introduced the Role entity with required properties and relationships to manage roles in the application. This includes RoleId, RoleName, and its association with Users. The entity is annotated for database mapping and schema consistency.

---
+++ HealthManagement.DAL/Entities/Role.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HealthManagement.Infrastructure.Entities;
+[Table("Role")]
+public class Role
+{
+    [Key]
+    public int RoleId { get; set; }
+    [Required]
+    public string? RoleName { get; set; }
+    
+    //Relations:
+    public virtual ICollection<User> Users { get; set; }
+}
