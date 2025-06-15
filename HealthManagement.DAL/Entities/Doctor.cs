c694952fe724675fbaa6911b91895ef84a7005e3
Saba-Burduli <sabagg790@gmail.com>
2025-06-15 19:46:54 +0400

    Add data annotations to Doctor entity for database mapping
    Applied [Key], [Table], and [ForeignKey] attributes to improve database schema mapping. These changes ensure that the Doctor table and relationships are accurately configured in the database.

--- HealthManagement.DAL/Entities/Doctor.cs
+++ HealthManagement.DAL/Entities/Doctor.cs
@@ -1,7 +1,12 @@
-﻿namespace HealthManagement.Infrastructure.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace HealthManagement.Infrastructure.Entities;
+[Table("Doctor")]
 public class Doctor
 {
+    [Key]
+    public int DoctorId { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public DateTime? DateOfBirth { get; set; }
@@ -9,6 +14,8 @@ public class Doctor
     public string? Email { get; set; }
     
     //Relations :
+    
+    [ForeignKey("Departament")]
     public int DepartmentId { get; set; }
     public virtual Departament? Departament { get; set; }
     
