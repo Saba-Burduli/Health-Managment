3a7a435a157a874e9809949036667707ba03dd75
Saba-Burduli <sabagg790@gmail.com>
2025-06-15 04:53:10 +0400

    Add PatientHealthInsurance entity to the data layer
    Introduce the PatientHealthInsurance entity with necessary properties and relationships, including a primary key and a reference to the Patient entity. This addition sets the foundation for managing patient health insurance data in the database.

---
+++ HealthManagement.DAL/Entities/PatientHealthInsurance.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata.Ecma335;
+
+namespace HealthManagement.Infrastructure.Entities;
+[Table("PatientHealthInsurance")]
+public class PatientHealthInsurance
+{
+    [Key]
+    public int HealthInsuranceId { get; set; }
+    
+    //Relations:
+    public int PatiendId { get; set; }
+    public virtual Patient? Patient { get; set; }
+}
