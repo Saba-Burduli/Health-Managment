c8364aa45bfaf0009992dc2d9aeea0c76524b2f5
Saba-Burduli <sabagg790@gmail.com>
2025-06-15 19:49:21 +0400

    Refactor PatientDiagnosisRepository to include DB context logic
    Updated the repository to inherit from BaseRepository and injected HealthManagementDBContext for proper database operations. Renamed methods to improve clarity and implemented data retrieval and addition functionality with null checks for robustness.

--- HealthManagement.DAL/Repositories/IPatientDiagnosisRepository.cs
+++ HealthManagement.DAL/Repositories/IPatientDiagnosisRepository.cs
@@ -1,23 +1,44 @@
 ﻿using HealthManagement.Infrastructure.Entities;
+using HealthManagement.Infrastructure.Infrastructures;
+using Microsoft.EntityFrameworkCore;
 
 namespace HealthManagement.Infrastructure.Repositories;
 
 public interface IPatientDiagnosisRepository
 {
-    Task<IEnumerable<PatientDiagnosis>> GetByPatientIdAsync(int patientDiagnosisId);
-    Task AddAsync(PatientDiagnosis diagnosis);
+    Task<IEnumerable<PatientDiagnosis>> GetPatientByIdAsync(int patientDiagnosisId);
+    Task AddPatientDiagnosisAsync(PatientDiagnosis diagnosis);
 }
 
-public class PatientDiagnosisRepository : IPatientDiagnosisRepository
+public class PatientDiagnosisRepository : BaseRepository<PatientDiagnosis>, IPatientDiagnosisRepository
 {
-    
-    public Task<IEnumerable<PatientDiagnosis>> GetByPatientIdAsync(int patientDiagnosisId)
+    private readonly HealthManagmentDBContext _context;
+
+    public PatientDiagnosisRepository(HealthManagmentDBContext context):base(context)
     {
-        throw new NotImplementedException();
+        _context = context;
+    }
+    
+    public async Task<IEnumerable<PatientDiagnosis>> GetPatientByIdAsync(int patientDiagnosisId)
+    {
+        if (_context==null||_context.PatientDiagnoses==null)
+        {
+            throw new ArgumentException("Arguments cannot be null");
+        }
+
+        return await _context.PatientDiagnoses
+            .Where(p => p.PatientDiagnosisId == patientDiagnosisId)
+            .ToListAsync();
     }
 
-    public Task AddAsync(PatientDiagnosis diagnosis)
+    //I can add (Task<PatientDiagnosis>)and use return
+    public async Task AddPatientDiagnosisAsync(PatientDiagnosis diagnosis)
     {
-        throw new NotImplementedException();
+        if (_context==null || _context.PatientDiagnoses==null)
+        {
+            throw new ArgumentException("Arguments cannot be null");
+        }
+        await _context.PatientDiagnoses.AddAsync(diagnosis);
+        await _context.SaveChangesAsync();
     }
 }
