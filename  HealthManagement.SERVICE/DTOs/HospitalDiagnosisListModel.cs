3ec3fde2d942a70cf7535f4c7c83f1401621eff0
Saba-Burduli <sabagg790@gmail.com>
2025-06-15 19:48:59 +0400

    Add HospitalDiagnosisListModel DTO
    Introduce a new DTO class, `HospitalDiagnosisListModel`, to represent laboratory details such as name, address, phone, and email. This provides a structured way to handle and transfer related data efficiently.

---
+++  HealthManagement.SERVICE/DTOs/HospitalDiagnosisListModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HealthManagement.SERVICE.DTOs;
+
+public class HospitalDiagnosisListModel
+{
+    public string? LaboratoryName { get; set; }
+    public string? LaboratoryAddress { get; set; }
+    public string? LaboratoryPhone { get; set; }
+    public string? LaboratoryEmail { get; set; }
+}
