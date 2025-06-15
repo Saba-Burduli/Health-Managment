20abcd83ada65b967ebb4f3b92426363b8ecb28e
Saba-Burduli <sabagg790@gmail.com>
2025-06-15 19:49:35 +0400

    Add PatientDiagnosisModel DTO
    Introduce a new DTO class, `PatientDiagnosisModel`, to represent diagnosis-related data. This class includes a property for the diagnosis date and references additional details in the main entity.

---
+++  HealthManagement.SERVICE/DTOs/PatientDiagnosisModel.cs
@@ -0,0 +1,7 @@
+﻿namespace HealthManagement.SERVICE.DTOs;
+
+public class PatientDiagnosisModel
+{
+    public DateTime? DateOfDiagnosis { get; set; }
+    //for more properties check main Entity of PatientDiagnosisModel
+}
