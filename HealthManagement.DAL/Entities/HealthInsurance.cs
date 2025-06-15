fe1808d9aaf3b4ef5cd0bd57447f6ac947f7f1bf
Saba-Burduli <sabagg790@gmail.com>
2025-06-15 04:52:33 +0400

    Add HealthInsurance entity to the data access layer
    Introduce the `HealthInsurance` entity with properties for company details. This entity includes a navigation property to manage relationships with `PatientHealthInsurance`. This change lays the groundwork for managing health insurance data.

---
+++ HealthManagement.DAL/Entities/HealthInsurance.cs
@@ -0,0 +1,13 @@
+﻿namespace HealthManagement.Infrastructure.Entities;
+
+public class HealthInsurance
+{
+    public string? CompanyName { get; set; }
+    public string? Address { get; set; }
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
+    
+    //Relations :
+    public virtual ICollection<PatientHealthInsurance>? PatientHealthInsurances { get; set; }
+    
+}
