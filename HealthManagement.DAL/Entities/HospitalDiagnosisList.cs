cb0376a7a5e357fd4af70f8578d4638b338b82c7
Saba-Burduli <sabagg790@gmail.com>
2025-06-15 04:52:55 +0400

    Add HospitalDiagnosisList entity to DAL
    Introduce the `HospitalDiagnosisList` entity in the data access layer to represent hospital and diagnosis-related information. This includes properties for laboratory details, a department relationship, and a list of patient diagnoses to establish necessary data associations.

---
+++ HealthManagement.DAL/Entities/HospitalDiagnosisList.cs
@@ -0,0 +1,16 @@
+﻿namespace HealthManagement.Infrastructure.Entities;
+
+public class HospitalDiagnosisList
+{
+    public string? laboratoryName { get; set; }
+    public string? laboratoryAddress { get; set; }
+    public string? laboratoryPhone { get; set; }
+    public string? laboratoryEmail { get; set; }
+    
+    //Relations:
+    
+    public int? DepartamentId { get; set; }
+    public virtual Departament? Departament { get; set; }
+    
+    public List<PatientDiagnosis>? PatientDiagnosis { get; set; }
+}
