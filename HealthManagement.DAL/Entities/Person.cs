7a9a6c22a83c07f0be7069a8bfef6c99540b4bf3
Saba-Burduli <sabagg790@gmail.com>
2025-06-15 04:53:20 +0400

    Add Person entity to the data access layer
    Introduced the `Person` entity with properties for ID, name, phone, and address, including data annotations for validation. This will serve as a foundational class for managing personal information in the system. A relationship with the `User` entity was also defined.

---
+++ HealthManagement.DAL/Entities/Person.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthManagement.Infrastructure.Entities;
+
+public class Person
+{
+    [Key]
+    public int PersonId { get; set; }
+        
+    [Required]
+    [MaxLength(20, ErrorMessage = "FistName is more than 20 letter")]
+    public string? FirstName { get; set; }
+
+    [Required]
+    [MaxLength(30, ErrorMessage = "LastName is more than 30 letter")]
+    public string? LastName { get; set; }
+
+    [Required]
+    [MaxLength(20, ErrorMessage = "Phone number is more than 20 letter")]
+    public string? Phone { get; set; }
+
+    [Required]
+    [MaxLength(50, ErrorMessage = "address is more than 50 letter")]
+    public string? Address { get; set; }
+
+    //Relations:
+    public virtual User? User { get; set; }
+}
