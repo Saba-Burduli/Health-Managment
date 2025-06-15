3420544261aaf2aebc4088f39db7b69dc425a6bf
Saba-Burduli <sabagg790@gmail.com>
2025-06-15 04:52:17 +0400

    Add Doctor entity to DAL
    Introduces the Doctor entity in the data access layer to represent doctors with basic attributes like Name, DateOfBirth, and Contact Info. Includes relationships with Department, PatientDiagnoses, and PatientHistories to establish database connections.

---
+++ HealthManagement.DAL/Entities/Doctor.cs
@@ -0,0 +1,18 @@
+﻿namespace HealthManagement.Infrastructure.Entities;
+
+public class Doctor
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
+    
+    //Relations :
+    public int DepartmentId { get; set; }
+    public virtual Departament? Departament { get; set; }
+    
+    public List<PatientDiagnosis>? PatientDiagnoses { get; set; }
+    
+    public List<PatientHistory>? PatimeHistories { get; set; }
+}
