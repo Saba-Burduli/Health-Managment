c06c1bda4628961a292fcb6d4103b33fc92a8426
Saba-Burduli <sabagg790@gmail.com>
2025-06-15 04:53:05 +0400

    Add PatientDiagnosis entity to the data access layer
    Introduce the PatientDiagnosis entity, establishing relationships with Patient, Doctor, PatientHistory, and HospitalDiagnosisList. This addition lays the groundwork for managing diagnosis data within the application.

---
+++ HealthManagement.DAL/Entities/PatientDiagnosis.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace HealthManagement.Infrastructure.Entities;
+public class PatientDiagnosis
+{
+    public int PatientDiagnosisId { get; set; }
+    public DateTime? DateifDiagnosis { get; set; } = DateTime.Now;
+    
+    //Relations:
+    public int PatientId { get; set; }
+    public virtual Patient? Patient { get; set; }
+
+    public int DoctorId { get; set; }
+    public virtual Doctor? Doctor { get; set; }
+    
+    public int PatientHistoryId { get; set; }
+    public virtual PatientHistory? PatientHistory { get; set; }
+    
+    public virtual HospitalDiagnosisList? HospitalDiagnosisList { get; set; }
+}
