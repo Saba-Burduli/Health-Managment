05bd3299558f2b880cf0403aedb423be1c47fd93
Saba-Burduli <sabagg790@gmail.com>
2025-06-15 04:52:13 +0400

    Add Departament entity to HealthManagement.DAL
    Introduces the Departament class with properties for basic details such as Name, Address, Phone, Email, and Notes. It also establishes relationships with HospitalDiagnosisLists and Doctors to support database modeling.

---
+++ HealthManagement.DAL/Entities/Departament.cs
@@ -0,0 +1,14 @@
+﻿namespace HealthManagement.Infrastructure.Entities;
+
+public class Departament
+{
+    public string? Name { get; set; }
+    public string? Address { get; set; }
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
+    public string? Notes { get; set; }
+    
+    //Relations:
+    public List<HospitalDiagnosisList>? HospitalDiagnosisLists { get; set; }
+    public List<Doctor>? Doctors { get; set; }
+}
