342b6445f6e93be44428feb9f339d2305f5f54f5
Saba-Burduli <sabagg790@gmail.com>
2025-06-15 19:46:41 +0400

    Add BaseRepository implementation for generic data access
    This commit introduces the BaseRepository class, providing generic CRUD operations for entity management using Entity Framework Core. It ensures proper context initialization and error handling for database operations, improving code reusability.

---
+++ HealthManagement.DAL/Infrastructures/BaseRepository.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HealthManagement.Infrastructure.Infrastructures;
+
+public class BaseRepository<T> : IBaseRepository<T> where T : class
+{
+      private readonly HealthManagmentDBContext _context;
+    private readonly DbSet<T> _dbSet;
+
+    public BaseRepository(HealthManagmentDBContext context)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(_context));
+        _dbSet = context.Set<T>() ?? throw new ArgumentNullException($"The context {nameof(context)} is null");
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        if (_context == null || _dbSet == null)
+        {
+            throw new InvalidOperationException("Database context is not initilized");
+        }
+        return await _dbSet.ToListAsync();
+    }
+    
+    public async Task<T> GetByIDAsync(int id)
+    {
+        if (_context == null || _dbSet == null)
+        {
+            throw new InvalidOperationException("Database context is not initilized");
+        }
+        return await _dbSet.FindAsync(id);
+    }
+
+    
+    public async Task AddAsync(T entity)
+    {
+        if (entity == null)
+        {
+            throw new ArgumentNullException($"{nameof(entity)} is null");
+        }
+
+        if (_context == null || _dbSet == null)
+        {
+            throw new InvalidOperationException("Database context is not initilized");
+        }
+         await _dbSet.AddAsync(entity);
+         await _context.SaveChangesAsync();//this is for save changes and paste it in _context class
+    }
+
+
+    
+    public Task UpdateAsync(T entity)
+    {
+        if (entity == null)
+        {
+            throw new NotImplementedException($"type {typeof(T).Name} is not implemented");
+        }
+
+        if (_context == null || _dbSet == null)
+        {
+            throw new InvalidOperationException("Database context is not initilized");
+        }
+        _dbSet.Update(entity);
+        return _context.SaveChangesAsync();
+    }
+    
+    
+    public async Task DeleteAsync(int id)
+    {
+        if (_context == null || _dbSet == null)
+            throw new InvalidOperationException("database context is not initilized");
+        
+        var user = await _context.Users.FindAsync(id);
+        _context.Remove(user);
+        await _context.SaveChangesAsync();
+    }
+    
+}
