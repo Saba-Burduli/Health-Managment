4c97b1456519278ca9424e51b458e1b34410ffec
Saba-Burduli <sabagg790@gmail.com>
2025-06-15 04:53:00 +0400

    Add Patient entity to data access layer
    Introduced the Patient entity with properties for personal details, relationships, and associated collections. This serves as a foundational model for handling patient-related data in the system.

---
+++ HealthManagement.DAL/Entities/Patient.cs
@@ -0,0 +1,20 @@
+﻿namespace HealthManagement.Infrastructure.Entities;
+
+public class Patient
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public DateTime? DateOfBirth { get; set; } 
+    public string? Address { get; set; }
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
+    
+    //Relations:
+    public int EmergencyContactId { get; set; }
+    public virtual EmergencyContact? EmergencyContact { get; set; }
+
+    public ICollection<PatientHealthInsurance>? PatientHealthInsurance { get; set; }
+    
+    public List<PatientHistory>? PatientHistory { get; set; }
+    
+}
