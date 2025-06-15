abe1649a147afaf9689b56cf9c23295efbafb5f7
Saba-Burduli <sabagg790@gmail.com>
2025-06-15 04:53:30 +0400

    Add User entity to the data access layer
    Introduced the User entity with necessary annotations for validation and database mapping. This will serve as a foundational model for user-related operations and relationships, including associations with Role and Person entities.

---
+++ HealthManagement.DAL/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HealthManagement.Infrastructure.Entities;
+[Table("User")]
+public class User
+{
+    [Key]
+    public int? UserId { get; set; }
+    [Required]
+    [MaxLength(20 , ErrorMessage = "UserName lenght is more than 20")]
+    public string? UserName { get; set; }
+    [Required]
+    [EmailAddress]
+    public string? Email { get; set; }
+    
+    //Relations :
+    public virtual Role? Role { get; set; } //we can also make this many to many (optional)
+    public virtual Person? Person { get; set; }
+}
