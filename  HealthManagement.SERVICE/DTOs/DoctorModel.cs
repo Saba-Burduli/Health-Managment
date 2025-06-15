e05832ed316c71e64661949f48f077712661a552
Saba-Burduli <sabagg790@gmail.com>
2025-06-15 19:46:59 +0400

    Add DoctorModel DTO to HealthManagement service
    Introduced a new DoctorModel class to represent doctor-related data in the service layer. This includes properties for FirstName, LastName, DateOfBirth, Phone, and Email, all as optional fields. This addition facilitates better organization and management of doctor information.

---
+++  HealthManagement.SERVICE/DTOs/DoctorModel.cs
@@ -0,0 +1,10 @@
+﻿namespace HealthManagement.SERVICE.DTOs;
+
+public class DoctorModel
+{ 
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
+}
