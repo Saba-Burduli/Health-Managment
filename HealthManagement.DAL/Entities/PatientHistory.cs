9627030e5f047582bc6fcf2b778c8b844525d2f6
Saba-Burduli <sabagg790@gmail.com>
2025-06-15 04:53:15 +0400

    Add PatientHistory entity to the data access layer
    Introduce the PatientHistory entity with necessary properties, relationships, and annotations. This entity links patients, doctors, and diagnoses, supporting visit history tracking in the database.

---
+++ HealthManagement.DAL/Entities/PatientHistory.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata.Ecma335;
+
+namespace HealthManagement.Infrastructure.Entities;
+[Table("PatientHistory")]
+public class PatientHistory
+{
+    [Required]
+    public DateTime DateOfVisit { get; set; } = DateTime.Now;
+    
+    //Relations:
+    public int DoctorId { get; set; } 
+    public virtual Doctor? Doctor { get; set; }
+
+    public int PatientId { get; set; }
+    public virtual Patient? Patient { get; set; }
+    
+    public List<PatientDiagnosis>? PatientDiagnoses { get; set; }
+    
+}
