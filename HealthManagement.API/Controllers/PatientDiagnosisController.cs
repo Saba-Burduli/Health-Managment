41740c2e8c8a4eb58dff009e32b1e2f5bac68a8f
SabaBurduli888 <sabagg790@gmail.com>
2025-06-16 15:17:27 +0400

    adding checkings for GetPatientHistorybyId method

--- HealthManagement.API/Controllers/PatientDiagnosisController.cs
+++ HealthManagement.API/Controllers/PatientDiagnosisController.cs
@@ -38,7 +38,8 @@ namespace HealthManagement.API.Controllers
         {
             if (ModelState.IsValid)
             {
-                var patientsDiagnosis = _patientDiagnosisService.GetPatientByDiagnosisIdAsync();
+                var patientsDiagnosis = _patientDiagnosisService.GetPatientByDiagnosisIdAsync(patientDiagnosisId);
+                
                 if (patientsDiagnosis == null || patientDiagnosisId == null)
                 {
                     return BadRequest("patientsDiagnosis or patientDiagnosisId cannot be null");
@@ -49,14 +50,41 @@ namespace HealthManagement.API.Controllers
 
             return BadRequest("Cannot founded patient Diagnosis By Id");
         }
-        
+
         [HttpGet("GetPatientHistorybyId")]
-        public  Task<IActionResult> GetPatientHistorybyId(int patientHistoryId);{
+        public async Task<IActionResult> GetPatientHistorybyId(int patientHistoryId)
+        {
             if (ModelState.IsValid)
             {
+                var patient = await _patientDiagnosisService.GetPatientHistorybyId(patientHistoryId);
                 
+                if (patient==null||patientHistoryId==null)
+                {
+                    return BadRequest("Patient or PatientHistoryId cannot be null");
+                }
+
+                return Ok("Patient history founded by Id Successfuly");
             }
+            
+            return BadRequest("Patient history Not founded by Id");
         }
-        //Task<PatientDiagnosis> AddDiagnosisToPatient(int patientDiagnosisId ,int patientId);
+
+        [HttpPost("AddDiagnosisToPatient")]
+        public async Task<IActionResult> AddDiagnosisToPatient(int patientDiagnosisId, int patientId)
+        {
+            if (ModelState.IsValid)
+            {
+                var patient = await _patientDiagnosisService.AddDiagnosisToPatient(patientDiagnosisId, patientId);
+                
+                if (patient==null || patientDiagnosisId==null || patientId==null)
+                {
+                    return BadRequest("patient or patientDiagnosisId or patientId cannot be null");
+                }
+                
+                return Ok();
+            }
+            
+            return BadRequest("Cannot add Diagnosis to Patient");
+        }
     }
 }
