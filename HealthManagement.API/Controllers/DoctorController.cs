d69d0ffe6991e9d4e8c0cba6870670e2a0d9235f
SabaBurduli888 <sabagg790@gmail.com>
2025-06-16 14:46:30 +0400

    adding inside DoctorController.cs DoctorService methods

--- HealthManagement.API/Controllers/DoctorController.cs
+++ HealthManagement.API/Controllers/DoctorController.cs
@@ -1,3 +1,4 @@
+using HealthManagement.SERVICE.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,5 +8,45 @@ namespace HealthManagement.API.Controllers
     [ApiController]
     public class DoctorController : ControllerBase
     {
+        private readonly DoctorService _doctorService;
+
+        public DoctorController(DoctorService doctorService)
+        {
+            _doctorService = doctorService;
+        }
+        
+        [HttpGet("GetALlDoctorsAsync")]
+        public async Task<IActionResult> GetALlDoctorsAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var doctor = await _doctorService.GetALlDoctorsAsync();
+                if (doctor==null)
+                {
+                    return BadRequest("Doctors cannot be null");
+                }
+
+                return Ok("Doctors founded Successfuly");
+            }
+
+            return BadRequest("Doctors not found");
+        }
+        
+        [HttpGet("GetDoctorByIdAsync")]
+        public async Task<IActionResult> GetDoctorByIdAsync(int doctorId)
+        {
+            if (ModelState.IsValid)
+            {
+                var doctor = await _doctorService.GetDoctorByIdAsync(doctorId);
+                if (doctor==null || doctorId ==null)
+                {
+                    return BadRequest("doctorId cannot be null");
+                }
+
+                return Ok();
+            }
+
+            return BadRequest();
+        }
     }
 }
